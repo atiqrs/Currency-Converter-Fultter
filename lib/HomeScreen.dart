eee3a4c081fd563c4eb5f457197d3cc91300c124
Atiqur <atiqrs.me@gmail.com>
2021-02-17 17:32:54 +0600

    2nd commit

--- lib/HomeScreen.dart
+++ lib/HomeScreen.dart
@@ -18,14 +18,17 @@ class _HomeScreenState extends State<HomeScreen> {
     color: Colors.redAccent,
   );
 
-  final List<String> currency = [
-    "UsDoller",
-    "CanadianDoller",
+  List currency = [
+    "Us Doller",
+    "Canadian Doller",
     "Taka",
     "Euro",
-    "StarlingPound",
-    "KsaReal",
+    "Starling Pound",
+    "Ksa Real",
   ];
+
+  var Apps = ["Apple", "Rahman"];
+  String AppsName = "";
 
   double inputValue;
   String fromCurrency = "UsDoller";
@@ -36,22 +39,75 @@ class _HomeScreenState extends State<HomeScreen> {
   Widget build(BuildContext context) {
     return SingleChildScrollView(
       child: Padding(
-        padding: const EdgeInsets.all(15),
-        child: Column(
-          children: [
+          padding: const EdgeInsets.all(15),
+          child: Column(children: [
+            Column(children: [
               TextField(
-                decoration: InputDecoration(
-                  labelText: "Input Your Doller Amount"
-                ),
-                onChanged: (inputValue){
+                decoration: InputDecoration(labelText: "Input Your Amount"),
+                onChanged: (inputValue) {
                   setState(() {
                     inputValue = inputValue;
+                    print(inputValue);
                   });
                 },
               ),
-          ],
-        )
-        ),
+
+              // DropdownButton(
+              //   value: AppsName,
+              //   onChanged: (String newValue) {
+              //     setState(() {
+              //       AppsName = newValue;
+              //     });
+              //   },
+              //   items: Apps.map((var e){
+              //     return DropdownMenuItem(value: e, child: Text(e));
+              //   }).toList(),
+              //   ),
+
+              // DropdownButton(
+              //   value: fromCurrency,
+              //   onChanged: (var newValue) {
+              //     setState(() {
+              //       fromCurrency = newValue;
+              //     });
+              //   },
+              //   items: currency.map((formCurrency) {
+              //     return DropdownMenuItem(
+              //         value: formCurrency, 
+              //         child: Text(formCurrency),
+              //         );
+              //   }).toList(),
+              // )
+            ]),
+            Column(children: [
+              TextField(
+                decoration:
+                    InputDecoration(labelText: "Input Your Taka Amount"),
+                onChanged: (inputValue) {
+                  setState(() {
+                    print(inputValue);
+                  });
+                },
+              ),
+              // DropdownButton(
+              //     value: toCurrency,
+              //     onChanged: (var newValue) {
+              //       setState(() {
+              //         fromCurrency = newValue;
+              //       });
+              //     },
+              //     items: currency.map((_toCurrency) {
+              //       return DropdownMenuItem(
+              //           value: _toCurrency, child: Text(_toCurrency));
+              //     }).toList())
+            ]),
+            Padding(
+              padding: const EdgeInsets.fromLTRB(0, 10, 0, 0),
+              child: RaisedButton(
+                child: Text("Button 1"),
+                onPressed: null),
+              )
+          ])),
     );
   }
 }
