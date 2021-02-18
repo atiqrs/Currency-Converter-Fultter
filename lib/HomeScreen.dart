fe9f1a67488ada13999593e57d549676c9dd4d79
Atiqur <atiqrs.me@gmail.com>
2021-02-18 15:26:30 +0600

    button and dropdown add

--- lib/HomeScreen.dart
+++ lib/HomeScreen.dart
@@ -1,4 +1,3 @@
-//press sst and select statefull / stateless widget
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
@@ -18,20 +17,17 @@ class _HomeScreenState extends State<HomeScreen> {
     color: Colors.redAccent,
   );
 
-  List currency = [
-    "Us Doller",
-    "Canadian Doller",
-    "Taka",
-    "Euro",
-    "Starling Pound",
-    "Ksa Real",
+  List<String> currency = <String>[
+    'Us Doller',
+    'Canadian Doller',
+    'Taka',
+    'Euro',
+    'Starling Pound',
+    'Ksa Real'
   ];
 
-  var Apps = ["Apple", "Rahman"];
-  String AppsName = "";
-
   double inputValue;
-  String fromCurrency = "UsDoller";
+  String fromCurrency = "Us Doller";
   String toCurrency = "Taka";
   String outputValue = "";
 
@@ -41,73 +37,122 @@ class _HomeScreenState extends State<HomeScreen> {
       child: Padding(
           padding: const EdgeInsets.all(15),
           child: Column(children: [
-            Column(children: [
-              TextField(
-                decoration: InputDecoration(labelText: "Input Your Amount"),
-                onChanged: (inputValue) {
-                  setState(() {
-                    inputValue = inputValue;
-                    print(inputValue);
-                  });
-                },
+            Container(
+              height: 100,
+              width: 100,
+              decoration: BoxDecoration(
+                border: Border.all(
+                  color: Colors.blue,
+                ),
+                borderRadius: BorderRadius.circular(10.0),
               ),
+              child: Center(
+                  child: Column(children: [
+                DropdownButton<String>(
+                  value: fromCurrency,
+                  onChanged: (String string) =>
+                      setState(() => fromCurrency = string),
+                  items: currency.map((String item) {
+                    return DropdownMenuItem<String>(
+                      child: Text('$item'),
+                      value: item,
+                    );
+                  }).toList(),
+                ),
+                TextField(
+                  decoration: InputDecoration(
+                      labelText: "Input your amount in $fromCurrency"),
+                  onChanged: (inputValue) {
+                    setState(() {
+                      inputValue = inputValue;
+                      print(inputValue);
+                    });
+                  },
+                ),
+              ])),
+            ),
 
-              // DropdownButton(
-              //   value: AppsName,
-              //   onChanged: (String newValue) {
-              //     setState(() {
-              //       AppsName = newValue;
-              //     });
-              //   },
-              //   items: Apps.map((var e){
-              //     return DropdownMenuItem(value: e, child: Text(e));
-              //   }).toList(),
-              //   ),
-
-              // DropdownButton(
-              //   value: fromCurrency,
-              //   onChanged: (var newValue) {
-              //     setState(() {
-              //       fromCurrency = newValue;
-              //     });
-              //   },
-              //   items: currency.map((formCurrency) {
-              //     return DropdownMenuItem(
-              //         value: formCurrency, 
-              //         child: Text(formCurrency),
-              //         );
-              //   }).toList(),
-              // )
-            ]),
-            Column(children: [
-              TextField(
-                decoration:
-                    InputDecoration(labelText: "Input Your Taka Amount"),
-                onChanged: (inputValue) {
-                  setState(() {
-                    print(inputValue);
-                  });
-                },
+            Container(
+              height: 150,
+              width: 600,
+              decoration: BoxDecoration(
+                border: Border.all(
+                  color: Colors.blue,
+                ),
+                borderRadius: BorderRadius.circular(10.0),
               ),
-              // DropdownButton(
-              //     value: toCurrency,
-              //     onChanged: (var newValue) {
-              //       setState(() {
-              //         fromCurrency = newValue;
-              //       });
-              //     },
-              //     items: currency.map((_toCurrency) {
-              //       return DropdownMenuItem(
-              //           value: _toCurrency, child: Text(_toCurrency));
-              //     }).toList())
-            ]),
+              child: Center(
+                  child: Column(children: [
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(5, 10, 5, 0),
+                  child: DropdownButton<String>(
+                    value: toCurrency,
+                    onChanged: (String string) =>
+                        setState(() => toCurrency = string),
+                    items: currency.map((String item) {
+                      return DropdownMenuItem<String>(
+                        child: Text('$item'),
+                        value: item,
+                      );
+                    }).toList(),
+                  ),
+                ),
+                Padding(
+                    padding: const EdgeInsets.fromLTRB(5, 10, 5, 0),
+                    child: TextFormField(
+                      decoration: InputDecoration(
+                          labelText: "Input your amount in $toCurrency"),
+                      onChanged: (inputValue) {
+                        setState(() {
+                          inputValue = inputValue;
+                          print(inputValue);
+                        });
+                      },
+                    ))
+              ])),
+            ),
+
+            // Column(children: [
+            //   TextField(
+            //     decoration:
+            //         InputDecoration(labelText: "Input Your Taka Amount"),
+            //     onChanged: (inputValue) {
+            //       setState(() {
+            //         print(inputValue);
+            //       });
+            //     },
+            //   ),
+            //   // DropdownButton(
+            //   //     value: toCurrency,
+            //   //     onChanged: (var newValue) {
+            //   //       setState(() {
+            //   //         fromCurrency = newValue;
+            //   //       });
+            //   //     },
+            //   //     items: currency.map((_toCurrency) {
+            //   //       return DropdownMenuItem(
+            //   //           value: _toCurrency, child: Text(_toCurrency));
+            //   //     }).toList())
+            // ]),
             Padding(
               padding: const EdgeInsets.fromLTRB(0, 10, 0, 0),
-              child: RaisedButton(
-                child: Text("Button 1"),
-                onPressed: null),
-              )
+              child: ElevatedButton(
+                  child: Text("Button"),
+                  onPressed: () {
+                    Scaffold.of(context).showSnackBar(SnackBar(
+                      content: Text('Tap'),
+                    ));
+                    print(toCurrency);
+                  }),
+
+            )
           ])),
     );
+  }
+
+  void snak() {
+    Scaffold.of(context).showSnackBar(SnackBar(
+      content: Text('Tap'),
+    ));
   }
 }
